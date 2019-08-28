d255a3b39c5313fbcfc745f0245c2540f9c1b72d
Husni Jabir <husnijabir@gmail.com>
2019-08-28 12:45:51 +0530

    Updated the Code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { devToolsEnhancer } from 'redux-devtools-extension';
 import VoteApp from './voteapp';
 import VoteResults from './components/voteresults';
 import myApp from './reducers';
-import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
-let store = createStore(myApp);
+let store = createStore(myApp, /* preloadedState, */ devToolsEnhancer());
 
 function render () {
   ReactDOM.render(
